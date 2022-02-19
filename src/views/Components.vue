18f38152a26ed0b2a62b960f28bc1d17c21d039a
Griffin McPherson <g.mcpherson@tcu.edu>
2022-02-19 17:59:07 -0600

    Initial website, still needs links attached

---
+++ src/views/Components.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <hero></hero>
+    <team></team>
+  </div>
+</template>
+<script>
+import Hero from "./components/Hero";
+import BasicElements from "./components/BasicElements";
+import team from "./components/team.vue";
+
+export default {
+  name: "components",
+  components: {
+    Hero,
+    BasicElements,
+    team,
+  },
+};
+</script>
---
+++ src/views/components/team.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="team-main-container m-3">
+    <h3>Meet the Team!</h3>
+    <div class="team-container mt-4">
+      <div>
+        <img
+          src="../../../public/img/headshots/griffin.jpeg"
+          alt=""
+          class="headshot"
+        />
+        <h2>Griffin McPherson</h2>
+      </div>
+      <div>
+        <img
+          src="../../../public/img/headshots/rajas.jpeg"
+          alt=""
+          class="headshot"
+        />
+        <h2>Rajas Nathak</h2>
+      </div>
+      <div>
+        <img
+          src="../../../public/img/headshots/shruti.jpeg"
+          alt=""
+          class="headshot"
+        />
+        <h2>Shruti Sharma</h2>
+      </div>
+      <div>
+        <img
+          src="../../../public/img/headshots/tyler.png"
+          alt=""
+          class="headshot"
+        />
+        <h2>Tyler Jacques</h2>
+      </div>
+      <div>
+        <img
+          src="../../../public/img/headshots/lucas.png"
+          alt=""
+          class="headshot"
+        />
+        <h2>Lucas Karwal</h2>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+import "../../assets/css/team.css";
+</script>
